146ac67de76ff62e22dd28554c488bdaae89faf7
soumyajitz <2014soumyajit@gmail.com>
2017-09-30 21:31:12 -0500

    Initial Commit

---
+++ server.js
@@ -0,0 +1,41 @@
+const Hapi = require('hapi');
+
+const server = new Hapi.Server();
+server.connection({ port: 4000 });
+
+server.route({
+    method: 'GET',
+    path: '/en',
+    handler: function(request, reply) {
+        reply('Hello');
+    }
+});
+
+const plugin = function(server, options, next) {
+    server.route({
+        method: 'GET',
+        path: '/cn',
+        handler: function(request, reply) {
+            reply('Hello China');
+        }
+    });
+    
+    next();
+};
+
+
+plugin.attributes = {
+    name: "My plugin"
+}
+
+server.register(plugin,(err) => {
+    if(err) 
+        throw err;
+
+    server.start((err) => {
+        if(err) {
+            throw err;
+        }
+        console.log("Server running at:", server.info.uri);
+    });
+});
